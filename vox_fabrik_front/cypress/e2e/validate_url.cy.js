2b05f3ccc365596a204350e3a1482d6c6118a193
Shalini Roy <spacetimedestroyed@gmail.com>
2024-11-23 20:51:50 -0800

    uncommented

--- vox_fabrik_front/cypress/e2e/validate_url.cy.js
+++ vox_fabrik_front/cypress/e2e/validate_url.cy.js
@@ -20,129 +20,66 @@ describe('Dataset Validation NodeJS API Tests', () => {
     });
   });
 
-  // it('[NGROK] should fail validation with a zip containing only metadata (dataset_metadata_only.zip)', () => {
-  //     cy.fixture('dataset_metadata_only.zip', 'binary')
-  //         .then(Cypress.Blob.binaryStringToBlob)
-  //         .then((blob) => {
-  //             const formData = new FormData();
-  //             formData.append('zipFile', blob, 'dataset_metadata_only.zip');
+  it('[LOCAL] should pass validation with a valid dataset (dataset_pass.zip)', () => {
+    cy.prepareFile('dataset_pass.zip', 'dataset_pass.zip', 'application/zip').then((file) => {
+      const formData = new FormData();
+      formData.append('zipFile', file);
+      cy.request({
+          method: 'POST',
+          url: apiUrlLocal,
+          headersLocal,
+          body: formData,
+          failOnStatusCode: false, // Prevent Cypress from failing immediately on errors
+      }).then((response) => {
+          cy.log(JSON.stringify(response.body)); // Log the response for debugging
+          expect(response.status).to.eq(200); // Adjust expected status
+      });
+  });
+  });
 
-  //             cy.request({
-  //                 method: 'POST',
-  //                 url: apiUrlNgrok,
-  //                 headersLocal,
-  //                 failOnStatusCode: false, // Don't fail Cypress on expected non-2xx responses
-  //                 body: formData,
-  //             }).then((response) => {
-  //                 expect(response.status).to.eq(400);
-  //                 expect(response.body).to.have.property('status', 'error');
-  //                 expect(response.body.message).to.include('must contain a "wavs" folder');
-  //             });
-  //         });
-  // });
+  it('[LOCAL] should fail validation with a zip containing only metadata (dataset_metadata_only.zip)', () => {
+      cy.prepareFile('dataset_metadata_only.zip', 'dataset_metadata_only.zip', 'application/zip').then((file) => {
+        const formData = new FormData();
+        formData.append('zipFile', file);
 
-  // it('[NGROK] should fail validation with a zip containing only wav files (dataset_wavs_only.zip)', () => {
-  //     cy.fixture('dataset_wavs_only.zip', 'binary')
-  //         .then(Cypress.Blob.binaryStringToBlob)
-  //         .then((blob) => {
-  //             const formData = new FormData();
-  //             formData.append('zipFile', blob, 'dataset_wavs_only.zip');
+        cy.request({
+            method: 'POST',
+            url: apiUrlLocal,
+            headersLocal,
+            failOnStatusCode: false,
+            body: formData,
+        }).then((response) => {
+          let parsedBody;
+          const textDecoder = new TextDecoder();
+          const jsonString = textDecoder.decode(response.body);
+          parsedBody = JSON.parse(jsonString);
 
-  //             cy.request({
-  //                 method: 'POST',
-  //                 url: apiUrlNgrok,
-  //                 headersLocal,
-  //                 failOnStatusCode: false,
-  //                 body: formData,
-  //             }).then((response) => {
-  //                 expect(response.status).to.eq(400);
-  //                 expect(response.body).to.have.property('status', 'error');
-  //                 expect(response.body.message).to.include('must contain a "metadata.csv" file');
-  //             });
-  //         });
-  // });
+          expect(response.status).to.eq(400);
+          expect(parsedBody).to.have.property('status', 'error');
+          expect(parsedBody.message).to.include('must contain a "wavs" folder');
+        });
+    });
+  });
 
-  // it('[NGROK] should fail validation with an invalid file type (dataset_not_zip.csv)', () => {
-  //     cy.fixture('dataset_not_zip.csv', 'binary')
-  //         .then(Cypress.Blob.binaryStringToBlob)
-  //         .then((blob) => {
-  //             const formData = new FormData();
-  //             formData.append('zipFile', blob, 'dataset_not_zip.csv');
-
-  //             cy.request({
-  //                 method: 'POST',
-  //                 url: apiUrlNgrok,
-  //                 headersLocal,
-  //                 failOnStatusCode: false,
-  //                 body: formData,
-  //             }).then((response) => {
-  //                 expect(response.status).to.eq(400);
-  //                 expect(response.body).to.have.property('status', 'error');
-  //                 expect(response.body.message).to.eq('Invalid file type. Please upload a zip file.');
-  //             });
-  //         });
-  // });
-  
-  // it('[LOCAL] should pass validation with a valid dataset (dataset_pass.zip)', () => {
-  //   cy.prepareFile('dataset_pass.zip', 'dataset_pass.zip', 'application/zip').then((file) => {
-  //     const formData = new FormData();
-  //     formData.append('zipFile', file);
-  //     cy.request({
-  //         method: 'POST',
-  //         url: apiUrlLocal,
-  //         headersLocal,
-  //         body: formData,
-  //         failOnStatusCode: false, // Prevent Cypress from failing immediately on errors
-  //     }).then((response) => {
-  //         cy.log(JSON.stringify(response.body)); // Log the response for debugging
-  //         expect(response.status).to.eq(200); // Adjust expected status
-  //     });
-  // });
-  // });
-
-  // it('[LOCAL] should fail validation with a zip containing only metadata (dataset_metadata_only.zip)', () => {
-  //     cy.prepareFile('dataset_metadata_only.zip', 'dataset_metadata_only.zip', 'application/zip').then((file) => {
-  //       const formData = new FormData();
-  //       formData.append('zipFile', file);
-
-  //       cy.request({
-  //           method: 'POST',
-  //           url: apiUrlLocal,
-  //           headersLocal,
-  //           failOnStatusCode: false,
-  //           body: formData,
-  //       }).then((response) => {
-  //         let parsedBody;
-  //         const textDecoder = new TextDecoder();
-  //         const jsonString = textDecoder.decode(response.body);
-  //         parsedBody = JSON.parse(jsonString);
-
-  //         expect(response.status).to.eq(400);
-  //         expect(parsedBody).to.have.property('status', 'error');
-  //         expect(parsedBody.message).to.include('must contain a "wavs" folder');
-  //       });
-  //   });
-  // });
-
-  // it('[LOCAL] should fail validation with a zip containing only wav files (dataset_wavs_only.zip)', () => {
-  //   cy.prepareFile('dataset_wavs_only.zip', 'dataset_wavs_only.zip', 'application/zip').then((file) => {
-  //     const formData = new FormData();
-  //     formData.append('zipFile', file);
-  //           cy.request({
-  //               method: 'POST',
-  //               url: apiUrlLocal,
-  //               headersLocal,
-  //               failOnStatusCode: false,
-  //               body: formData,
-  //           }).then((response) => {
-  //             let parsedBody;
-  //             const textDecoder = new TextDecoder();
-  //             const jsonString = textDecoder.decode(response.body);
-  //             parsedBody = JSON.parse(jsonString);
-  //             expect(response.status).to.eq(400);
-  //             expect(parsedBody).to.have.property('status', 'error');
-  //             expect(parsedBody.message).to.include('must contain a "metadata.csv" file');
-  //           });
-  //       });
-  // });
+  it('[LOCAL] should fail validation with a zip containing only wav files (dataset_wavs_only.zip)', () => {
+    cy.prepareFile('dataset_wavs_only.zip', 'dataset_wavs_only.zip', 'application/zip').then((file) => {
+      const formData = new FormData();
+      formData.append('zipFile', file);
+            cy.request({
+                method: 'POST',
+                url: apiUrlLocal,
+                headersLocal,
+                failOnStatusCode: false,
+                body: formData,
+            }).then((response) => {
+              let parsedBody;
+              const textDecoder = new TextDecoder();
+              const jsonString = textDecoder.decode(response.body);
+              parsedBody = JSON.parse(jsonString);
+              expect(response.status).to.eq(400);
+              expect(parsedBody).to.have.property('status', 'error');
+              expect(parsedBody.message).to.include('must contain a "metadata.csv" file');
+            });
+        });
+  });
 });
