2225231ca9d83a57f63ae8c25aac73cc0ab8cbae
Shalini Roy <spacetimedestroyed@gmail.com>
2024-12-05 21:05:18 -0800

    Removed unused BUtton

--- vox_fabrik_front/src/components/HomePageButtons.js
+++ vox_fabrik_front/src/components/HomePageButtons.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Button, Stack } from '@mui/material';
+import { Stack } from '@mui/material';
 import CreateVoiceModels from './CreateVoiceModels'; // Corrected import
 import VoiceModels from './VoiceModels'; // Corrected import
 
